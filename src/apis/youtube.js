a4ddd8a482b9ad5497970572a708f22bbe35ef6b
Eullin Chang <42239809+Eullin@users.noreply.github.com>
2019-02-13 19:28:37 +0100

    Update youtube.js

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const KEY = "AIzaSyA3XLGyYViABwwbMZR9dAr6wGbjZUyDvPw";
+const KEY = "hidden";
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
